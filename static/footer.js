0d9fdfcf2b5da4a06e9aaa48414606a7354eee3c
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-02 14:13:27 -0300

    added javascript/css lib for templates

---
+++ static/footer.js
@@ -0,0 +1,4 @@
+BANNER = `<div>
+    <h3>CopyRight &copy; Yaks Vinicios S. Souza <vinicios.sousa909@gmail.com></h3>
+</div>`;
+document.write(BANNER)
---
+++ static/header.js
@@ -0,0 +1,15 @@
+BANNER = `<div id="title">
+    <img src="/static/favicon.png" alt="pirate code logo PNG"/>
+    <h1>Pirate Code</h1>
+</div>
+<div id="menu">
+    <div id="back">
+    <nav id="buttons">
+        <a href="/"><li class="${(window.location.pathname == "/")? "select" : "normal"}">home</li></a>
+        <a href="/projects"><li class="${(window.location.pathname == "/projects")? "select" : "normal"}">projects</li></a>
+        <a href="/about"><li class="${(window.location.pathname == "/about")? "select" : "normal"}">about</li></a>
+        <a href="/contact"><li class="${(window.location.pathname == "/contact")? "select" : "normal"}">contact</li></a>
+    </nav>
+    </div>
+</div>`;
+document.write(BANNER)

6645c03c7486d2f3854475ad8bcb90f37539257e
RoboCopGay <vinicios.sousa909@gmail.com>
2020-05-03 17:13:14 -0300

    adding the user register function

--- pirate_code/__init__.py
+++ pirate_code/__init__.py
@@ -2,13 +2,32 @@ from flask import Flask, request, render_template as render, jsonify
 
 pirate = Flask('Pirate Code')
 
-from pirate_code.database import pirate_db
+from pirate.database import pirate_db, hash
 @pirate.route('/', methods = ['GET'])
 def index():
     return 'Hello World'
 
+@pirate.route('/sign-up', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render('register.html')
+
+    data = request.json
+    usernames = list(pirate_db.db.user.find({'username': data['username']}))
+    emails = list(pirate_db.db.user.find({'email': data['email']}))
+
+    if emails == [] and usernames == []:
+        data['password'] = hash(data['password'])
+
+        user_id = pirate_db.db.user.insert_one(data).inserted_id
+        if 'json' in request.mimetype:
+            return jsonify({ 'success': True })
+    else:
+        if 'json' in request.mimetype:
+            return jsonify({ 'success': False }), 500
+        return render('register.html', warning = 'Your email or username did registered!'), 500
+
+
 @pirate.route('/editor', methods = ['GET'])
 def editor():
-    return render(
-        'editor.html'
-    )
+    return render('editor.html')
--- pirate_code/database.py
+++ pirate_code/database.py
@@ -1,5 +1,10 @@
 from flask_pymongo import PyMongo
-from pirate_code import pirate
+from hashlib import sha512
+from pirate import pirate
+
+def hash(string):
+    return sha512(bytes(string, 'utf-8')).hexdigest()
+
 
 pirate.config["MONGO_URI"] = "mongodb://localhost:27017/PirateCode"
 pirate_db = PyMongo(pirate)
